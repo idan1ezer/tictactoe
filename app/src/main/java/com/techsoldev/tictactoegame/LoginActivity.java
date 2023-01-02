87c65228df612fd244f35cd2fbd15b2e9c63cfb4
Idan <idanez.chatbot@gmail.com>
2023-01-02 19:34:58 +0200

    added login 2

--- app/src/main/java/com/techsoldev/tictactoegame/LoginActivity.java
+++ app/src/main/java/com/techsoldev/tictactoegame/LoginActivity.java
@@ -1,10 +1,25 @@
 package com.techsoldev.tictactoegame;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textview.MaterialTextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout start_EDT_email;
+    private TextInputLayout start_EDT_password;
+    private MaterialButton start_BTN_login;
+    private MaterialTextView start_TXT_forgot;
+    private TextInputLayout[] loginFields;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +28,63 @@ public class LoginActivity extends AppCompatActivity {
 
         //Cade-Hub
 
+        findViews();
+        initBTNs();
 
+
+    }
+
+
+
+    private void initBTNs() {
+
+        start_BTN_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { login();}
+        });
+
+    }
+
+
+    private void login() {
+        if (isValid()) {
+            String email, password;
+            email = start_EDT_email.getEditText().getText().toString().trim();
+            password = start_EDT_password.getEditText().getText().toString().trim();
+
+            // on success:
+            finish();
+            Intent intent = new Intent(LoginActivity.this, OfflineGameMenuActivity.class);
+            startActivity(intent);
+
+        }
+
+        else
+            Toast.makeText(this, "One or more field are invalid!", Toast.LENGTH_LONG).show();
+
+    }
+
+
+    private boolean isValid() {
+        for (TextInputLayout field : loginFields) {
+            if (field.getError() != null || field.getEditText().getText().toString().isEmpty())
+                return false;
+        }
+        return true;
+    }
+
+
+
+
+
+    private void findViews() {
+        start_EDT_email = findViewById(R.id.start_EDT_email);
+        start_EDT_password = findViewById(R.id.start_EDT_password);
+        start_BTN_login = findViewById(R.id.start_BTN_login);
+        start_TXT_forgot = findViewById(R.id.start_TXT_forgot);
+        loginFields = new TextInputLayout[] {
+                start_EDT_email,
+                start_EDT_password
+        };
     }
 }
