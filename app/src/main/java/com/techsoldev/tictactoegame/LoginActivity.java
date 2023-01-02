8f57c7b15d27e82ba5a1f66c9f95b4ff877e0a5e
Idan <idanez.chatbot@gmail.com>
2023-01-02 19:30:02 +0200

    added login

---
+++ app/src/main/java/com/techsoldev/tictactoegame/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.techsoldev.tictactoegame;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Cade-Hub
+
+
+    }
+}
